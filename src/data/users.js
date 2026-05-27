c09e667079496ba76e59ffccbb52c5953cfa356d
maisaidrpetr-maker <maisaidrpetr@gmail.com>
2026-05-28 01:06:37 +0200

    Login

---
+++ src/data/users.js
@@ -0,0 +1,9 @@
+// src/data/users.js
+export const USERS = {
+  "petrm": "Petr Maišaidr (Kytara)",
+  "jank": "Honza Kolář (Mandolína)",
+  "janan": "Jana Novotná (Housle)",
+  "zuzkab": "Zuzana Bartoňová (Zpěv)",
+  "zbynekb": "Zbyněk Bartoň (Banjo)",
+  "martink": "Martin Král (Kontrabas)"
+};
